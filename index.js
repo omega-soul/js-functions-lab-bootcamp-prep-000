85e89f5320ec232bbe1319daebe556646ed05605
vincent <omegaortiz1997@gmail.com>
2018-02-05 19:07:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ function happyHolidays() {
   return (`Happy holidays!`);
 }
 
+function happyHolidaysTo(name) {
+  return (`Happy holidays, ${name}!`);
+}
+happyHolidaysTo("Vincent");
 
 function happyHolidaysTo(holiday, name) {
   return (`Happy ${holiday}, ${name}!`);
