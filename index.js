0c19c825101425edd48b101ca5921f7eff9b4ee5
vincent <omegaortiz1997@gmail.com>
2018-02-05 19:27:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function happyHolidayTo(holiday, name) {
 }
 happyHolidayTo("Birthday", "Vincent");
 
-function holidayCountdown(days, holiday) {
+function holidayCountdown(holiday, days) {
   return (`It\'s ${days} days until ${holiday}!}`);
 }
 holidayCountdown("20", "Mother\'s");
